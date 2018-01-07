bd1554e07bdf69394a84aeb74440d0a030002de7
Abhi defstryker <defstryker@hotmail.com>
2018-01-07 19:16:10 +0530

    tweaked fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,9 @@ end
 # Base16 Shell
 if status --is-interactive
     eval sh $HOME/.config/base16-shell/scripts/base16-monokai.sh
+
+    set -xg NIX_LINK "$HOME/.nix-profile"
+
 	set PATH $PATH ~/.npm-packages/bin
 	# set PATH $PATH /home/defstryker/.gem/ruby/2.4.0/bin
 	set PATH $PATH ~/.cargo/bin/
@@ -17,6 +20,8 @@ if status --is-interactive
 	set -g -x PATH  $PATH /home/defstryker/.go/bin/
 	# set -g -x PATH  $PATH /home/defstryker/.npm/bin/
 	set -g -x GTK2_RC_FILES "/home/defstryker/.gtkrc-2.0"
+    set -xg PATH $PATH $NIX_LINK/bin
+    eval fish ~/.config/fish/functions/nix.fish
 end
 
 function p
